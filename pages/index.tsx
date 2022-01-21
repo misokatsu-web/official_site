0d44c386a000785b79c7b0a9c1ed98a8000675fa
ryosukeYamazaki <ryamazaki@c-fo.com>
2022-01-21 22:18:20 +0900

    :+1: トップの文言で不要なものを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
 import Post from '../types/post'
 
 type Props = {
@@ -19,7 +18,7 @@ const Index = ({ allPosts }: Props) => {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>みそかつウェブは名古屋のウェブエンジニアによる技術コミュニティです。</title>
         </Head>
         <Container>
           <Intro />
